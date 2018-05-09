42fbc23e6c59553b1c375b888e9d97c9c75b7665
DUONGTRUONG-986\Duong Truong <johnangel135@gmail.com>
2018-05-09 01:44:55 -0500

    update

--- src/test/java/demo/testcases/LoginChrome.java
+++ src/test/java/demo/testcases/LoginChrome.java
@@ -1,12 +1,12 @@
-//package demo.testcases;
-//
-//import demo.helper.TestData;
-//
-//public class LoginChrome extends Login {
-//    private final String DATA_PATH = "testdata/loginchrome.csv";
-//
-//    @Override
-//    void init(){
-//        TestData.readData(DATA_PATH);
-//    }
-//}
+package demo.testcases;
+
+import demo.helper.TestData;
+
+public class LoginChrome extends Login {
+    private final String DATA_PATH = "testdata/loginchrome.csv";
+
+    @Override
+    void init(){
+        TestData.readData(DATA_PATH);
+    }
+}
